4575cab68bb81bcc7cdbd29456a5ad08b536ef2c
Kryvokhyzha Vyacheslav <hiki_student@ukr.net>
2019-04-23 07:53:16 +0300

    Add GameEngine.Test project

---
+++ GameEngine.Tests/Core/SettingsManagerTest.cs
@@ -0,0 +1,29 @@
+﻿using GameEngine.Core;
+using GameEngine.GameObjects.ServiceObjects;
+using GameEngine.Interfaces.DataAccess;
+using GameEngine.Interfaces.Storages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GameEngine.Tests.Core
+{
+    [TestClass]
+    public class SettingsManagerTest
+    {
+        [TestMethod]
+        public void GetSettings_StorageAndProviderReturn_SettingsReturned()
+        {
+            var settings = new Settings();
+
+            var settingsProvider = Mock.Of<ISettingsProvider>(s => s.Get() == settings);
+            var storage = Mock.Of<IDataStorage>(d => d.GetValue<Settings>("CurrentSettingsKey") == settings);
+
+            var settingsManager = new SettingsManager(settingsProvider, storage);
+
+            var result = settingsManager.GetSettings();
+
+            Assert.AreEqual(settings, result);
+            Assert.IsNotNull(result);
+        }
+    }
+}
